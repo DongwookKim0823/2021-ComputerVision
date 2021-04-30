8f79d33b80d64fa7d49059c355ff797e6e25cf42
DongwookKim0823 <dwkim0823@naver.com>
2021-04-30 14:19:11 +0900

    과제04

---
+++ Homework4(동공영역 외접사각형 및 무게중심통과하는 크로스라인 그리기).cpp
@@ -0,0 +1,546 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+void InverseImage(BYTE* Img, BYTE *Out, int W, int H)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++)
+	{
+		Out[i] = 255 - Img[i];
+	}
+}
+void BrightnessAdj(BYTE* Img, BYTE* Out, int W, int H, int Val)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++)
+	{
+		if (Img[i] + Val > 255)
+		{
+			Out[i] = 255;
+		}
+		else if (Img[i] + Val < 0)
+		{
+			Out[i] = 0;
+		}
+		else 	Out[i] =Img[i] + Val;
+	}
+}
+void ContrastAdj(BYTE* Img, BYTE* Out, int W, int H, double Val)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++)
+	{
+		if (Img[i] * Val > 255.0)
+		{
+			Out[i] = 255;
+		}
+		else 	Out[i] = (BYTE)(Img[i] * Val);
+	}
+}
+
+void ObtainHistogram(BYTE* Img, int* Histo, int W, int H)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++) {
+		Histo[Img[i]]++;
+	}
+}
+
+void ObtainAHistogram(int* Histo, int* AHisto)
+{
+	for (int i = 0; i < 256; i++) {
+		for (int j = 0; j <= i; j++) {
+			AHisto[i] += Histo[j];
+		}
+	}
+}
+
+void HistogramStretching(BYTE * Img, BYTE * Out, int * Histo, int W, int H)
+{
+	int ImgSize = W * H;
+	BYTE Low, High;
+	for (int i = 0; i < 256; i++) {
+		if (Histo[i] != 0) {
+			Low = i;
+			break;
+		}
+	}
+	for (int i = 255; i >= 0; i--) {
+		if (Histo[i] != 0) {
+			High = i;
+			break;
+		}
+	}
+	for (int i = 0; i < ImgSize; i++) {
+		Out[i] = (BYTE)((Img[i] - Low) / (double)(High - Low) * 255.0);
+	}
+}
+void HistogramEqualization(BYTE* Img, BYTE* Out, int* AHisto, int W, int H)
+{
+	int ImgSize = W * H;
+	int Nt = W * H, Gmax = 255;
+	double Ratio = Gmax / (double)Nt;
+	BYTE NormSum[256];
+	for (int i = 0; i < 256; i++) {
+		NormSum[i] = (BYTE)(Ratio * AHisto[i]);
+	}
+	for (int i = 0; i < ImgSize; i++)
+	{
+		Out[i] = NormSum[Img[i]];
+	}
+}
+
+void Binarization(BYTE * Img, BYTE * Out, int W, int H, BYTE Threshold)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++) {
+		if (Img[i] < Threshold) Out[i] = 0;
+		else Out[i] = 255;
+	}
+}
+
+//int GozalezBinThresh()
+//{
+//
+//}
+
+void AverageConv(BYTE* Img, BYTE* Out, int W, int H) // 박스평활화
+{
+	double Kernel[3][3] = {0.11111, 0.11111, 0.11111,
+										0.11111, 0.11111, 0.11111,
+										0.11111, 0.11111, 0.11111	};
+	double SumProduct = 0.0;
+	for (int i = 1; i < H-1; i++) { // Y좌표 (행)
+		for (int j = 1; j < W-1; j++) { // X좌표 (열)
+			for (int m = -1; m <= 1; m++) { // Kernel 행
+				for (int n = -1; n <= 1; n++) { // Kernel 열
+					SumProduct += Img[(i+m)*W + (j+n)] * Kernel[m+1][n+1];
+				}
+			}
+			Out[i * W + j] = (BYTE)SumProduct;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void GaussAvrConv(BYTE* Img, BYTE* Out, int W, int H) // 가우시안평활화
+{
+	double Kernel[3][3] = {0.0625, 0.125, 0.0625,
+										0.125, 0.25, 0.125,
+										0.0625, 0.125, 0.0625 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // Y좌표 (행)
+		for (int j = 1; j < W - 1; j++) { // X좌표 (열)
+			for (int m = -1; m <= 1; m++) { // Kernel 행
+				for (int n = -1; n <= 1; n++) { // Kernel 열
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			Out[i * W + j] = (BYTE)SumProduct;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Prewitt_X_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt 마스크 X
+{
+	double Kernel[3][3] = { -1.0, 0.0, 1.0,
+										-1.0, 0.0, 1.0,
+										-1.0, 0.0, 1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // Y좌표 (행)
+		for (int j = 1; j < W - 1; j++) { // X좌표 (열)
+			for (int m = -1; m <= 1; m++) { // Kernel 행
+				for (int n = -1; n <= 1; n++) { // Kernel 열
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 765  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 3;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Prewitt_Y_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt 마스크 X
+{
+	double Kernel[3][3] = { -1.0, -1.0, -1.0,
+										0.0, 0.0, 0.0,
+										1.0, 1.0, 1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // Y좌표 (행)
+		for (int j = 1; j < W - 1; j++) { // X좌표 (열)
+			for (int m = -1; m <= 1; m++) { // Kernel 행
+				for (int n = -1; n <= 1; n++) { // Kernel 열
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 765  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 3;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Sobel_X_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt 마스크 X
+{
+	double Kernel[3][3] = { -1.0, 0.0, 1.0,
+										-2.0, 0.0, 2.0,
+										-1.0, 0.0, 1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // Y좌표 (행)
+		for (int j = 1; j < W - 1; j++) { // X좌표 (열)
+			for (int m = -1; m <= 1; m++) { // Kernel 행
+				for (int n = -1; n <= 1; n++) { // Kernel 열
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 1020  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 4;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Sobel_Y_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt 마스크 X
+{
+	double Kernel[3][3] = { -1.0, -2.0, -1.0,
+										0.0, 0.0, 0.0,
+										1.0, 2.0, 1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // Y좌표 (행)
+		for (int j = 1; j < W - 1; j++) { // X좌표 (열)
+			for (int m = -1; m <= 1; m++) { // Kernel 행
+				for (int n = -1; n <= 1; n++) { // Kernel 열
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 765  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 4;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Laplace_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt 마스크 X
+{
+	double Kernel[3][3] = { -1.0, -1.0, -1.0,
+										-1.0, 8.0, -1.0,
+										-1.0, -1.0, -1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // Y좌표 (행)
+		for (int j = 1; j < W - 1; j++) { // X좌표 (열)
+			for (int m = -1; m <= 1; m++) { // Kernel 행
+				for (int n = -1; n <= 1; n++) { // Kernel 열
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 2040  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 8;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Laplace_Conv_DC(BYTE* Img, BYTE* Out, int W, int H) // Prewitt 마스크 X
+{
+	double Kernel[3][3] = { -1.0, -1.0, -1.0,
+										-1.0, 9.0, -1.0,
+										-1.0, -1.0, -1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // Y좌표 (행)
+		for (int j = 1; j < W - 1; j++) { // X좌표 (열)
+			for (int m = -1; m <= 1; m++) { // Kernel 행
+				for (int n = -1; n <= 1; n++) { // Kernel 열
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			//Out[i * W + j] = abs((long)SumProduct) / 8;
+			if (SumProduct > 255.0) Out[i * W + j] = 255;
+			else if (SumProduct < 0.0) Out[i * W + j] = 0;
+			else Out[i * W + j] = (BYTE)SumProduct;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void SaveBMPFile(BITMAPFILEHEADER hf, BITMAPINFOHEADER hInfo, 
+	RGBQUAD* hRGB, BYTE* Output, int W, int H, const char* FileName)
+{
+	FILE * fp = fopen(FileName, "wb");
+	fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp);
+	fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+	fwrite(Output, sizeof(BYTE), W*H, fp);
+	fclose(fp);
+}
+
+void swap(BYTE* a, BYTE* b) 
+{
+	BYTE temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+BYTE Median(BYTE* arr, int size)
+{
+	// 오름차순 정렬
+	const int S = size;
+	for (int i = 0; i < size - 1; i++) // pivot index
+	{
+		for (int j = i + 1; j < size; j++) // 비교대상 index
+		{
+			if (arr[i] > arr[j]) 	swap(&arr[i], &arr[j]);
+		}
+	}
+	return arr[S/2];
+}
+
+BYTE MaxPooling(BYTE* arr, int size)
+{
+	// 오름차순 정렬
+	const int S = size;
+	for (int i = 0; i < size - 1; i++) // pivot index
+	{
+		for (int j = i + 1; j < size; j++) // 비교대상 index
+		{
+			if (arr[i] > arr[j]) 	swap(&arr[i], &arr[j]);
+		}
+	}
+	return arr[S-1];
+}
+
+BYTE MinPooling(BYTE* arr, int size)
+{
+	// 오름차순 정렬
+	const int S = size;
+	for (int i = 0; i < size - 1; i++) // pivot index
+	{
+		for (int j = i + 1; j < size; j++) // 비교대상 index
+		{
+			if (arr[i] > arr[j]) 	swap(&arr[i], &arr[j]);
+		}
+	}
+	return arr[0];
+}
+
+int push(short* stackx, short* stacky, int arr_size, short vx, short vy, int* top)
+{
+	if (*top >= arr_size) return(-1);
+	(*top)++;
+	stackx[*top] = vx;
+	stacky[*top] = vy;
+	return(1);
+}
+
+int pop(short* stackx, short* stacky, short* vx, short* vy, int* top)
+{
+	if (*top == 0) return(-1);
+	*vx = stackx[*top];
+	*vy = stacky[*top];
+	(*top)--;
+	return(1);
+}
+
+
+// GlassFire 알고리즘을 이용한 라벨링 함수
+void m_BlobColoring(BYTE* CutImage, int height, int width)
+{
+	int i, j, m, n, top, area, Out_Area, index, BlobArea[1000];
+	long k;
+	short curColor = 0, r, c;
+	//	BYTE** CutImage2;
+	Out_Area = 1;
+
+
+	// 스택으로 사용할 메모리 할당
+	short* stackx = new short[height * width];
+	short* stacky = new short[height * width];
+	short* coloring = new short[height * width];
+
+	int arr_size = height * width;
+
+	// 라벨링된 픽셀을 저장하기 위해 메모리 할당
+
+	for (k = 0; k < height * width; k++) coloring[k] = 0;  // 메모리 초기화
+
+	for (i = 0; i < height; i++)
+	{
+		index = i * width;
+		for (j = 0; j < width; j++)
+		{
+			// 이미 방문한 점이거나 픽셀값이 255가 아니라면 처리 안함
+			if (coloring[index + j] != 0 || CutImage[index + j] != 255) continue;
+			r = i; c = j; top = 0; area = 1;
+			curColor++;
+
+			while (1)
+			{
+			GRASSFIRE:
+				for (m = r - 1; m <= r + 1; m++)
+				{
+					index = m * width;
+					for (n = c - 1; n <= c + 1; n++)
+					{
+						//관심 픽셀이 영상경계를 벗어나면 처리 안함
+						if (m < 0 || m >= height || n < 0 || n >= width) continue;
+
+						if ((int)CutImage[index + n] == 255 && coloring[index + n] == 0)
+						{
+							coloring[index + n] = curColor; // 현재 라벨로 마크
+							if (push(stackx, stacky, arr_size, (short)m, (short)n, &top) == -1) continue;
+							r = m; c = n; area++;
+							goto GRASSFIRE;
+						}
+					}
+				}
+				if (pop(stackx, stacky, &r, &c, &top) == -1) break;
+			}
+			if (curColor < 1000) BlobArea[curColor] = area;
+		}
+	}
+
+	float grayGap = 255.0f / (float)curColor;  //curColor는 덩어리 져있는 component 개수
+
+	// 가장 면적이 넓은 영역을 찾아내기 위함 
+	for (i = 1; i <= curColor; i++)   //curColor는 component 개수
+	{
+		if (BlobArea[i] >= BlobArea[Out_Area]) Out_Area = i; //i번 째 component사이즈를 여태 가장 큰 사이즈와 비교해서 같거나 크면 갱신
+	}
+	// CutImage 배열 클리어~
+	for (k = 0; k < width * height; k++) CutImage[k] = 255; //CutImage는 main함수에서 받아온 output배열, 모두 하얗게 채움
+
+	// coloring에 저장된 라벨링 결과중 (Out_Area에 저장된) 영역이 가장 큰 것만 CutImage에 저장
+	for (k = 0; k < width * height; k++)
+	{
+		if (coloring[k] == Out_Area) CutImage[k] = 0;  // 가장 큰 것만 저장(size filtering) //coloring[k]에는 인덱스 정보만 들어가 있음
+		//if (BlobArea[coloring[k]] > 500) CutImage[k] = 0;  // 특정 면적 이상 되는 영역만 출력
+		//CutImage[k] = (unsigned char)(coloring[k] * grayGap);  //전경의 각각의 component들을 grayGap으로 채워나감, 모든 component 확인
+	}
+
+	delete[] coloring;
+	delete[] stackx;
+	delete[] stacky;
+}
+// 라벨링 후 가장 넓은 영역에 대해서만 뽑아내는 코드 포함
+
+void BinaryImageEdgeDetection(BYTE* Bin, BYTE* Out, int W, int H) {
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			if (Bin[i * W + j] == 0) //전경화소라면(0이면 가장 어두운 것. 위의 코드에서 가장 큰 동전을 이진화해서 Temp에 저장했음)
+			{
+				if (!(Bin[(i - 1) * W + j] == 0 && Bin[(i + 1) * W + j] == 0 &&
+					Bin[i * W + (j - 1)] == 0 && Bin[i * W + (j + 1)] == 0)) //4방향 화소 중 하나라도 전경이 아니라면
+					Out[i * W + j] = 255;
+			}
+		}
+	}
+}
+
+void DrawRectOutline(BYTE* Img, int W, int H, int x1, int x2, int y1, int y2) {
+	for (int i = x1; i <= x2; i++) {
+		for (int j = 0; j < H; j++) {
+			if (j == y1 || j == y2)
+				Img[j*W + i] = 255;
+		}
+	}
+
+	for (int i = y2; i <= y1; i++) {
+		for (int j = 0; j < W; j++) {
+			if (j == x1 || j == x2)
+				Img[i * W + j] = 255;
+		}
+	}
+}
+
+void DrawCrossLine(BYTE* Img, int W, int H, int Cx, int Cy) {
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			if(i==Cy) Img[i * W + j] = 255;
+			if (j == Cx) Img[i * W + j] = 255;
+		}
+	}
+}
+
+int main()
+{
+	BITMAPFILEHEADER hf; // 14바이트
+	BITMAPINFOHEADER hInfo; // 40바이트
+	RGBQUAD hRGB[256]; // 1024바이트
+	FILE* fp;
+	fp = fopen("pupil2.bmp", "rb");
+	if (fp == NULL) {
+		printf("File not found!\n");
+		return -1;
+	}
+	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+	fread(&hInfo, sizeof(BITMAPINFOHEADER), 1, fp);
+	fread(hRGB, sizeof(RGBQUAD), 256, fp);
+	int ImgSize = hInfo.biWidth * hInfo.biHeight;
+	BYTE * Image = (BYTE *)malloc(ImgSize);
+	BYTE * Temp = (BYTE*)malloc(ImgSize); // 임시배열
+	BYTE* Output = (BYTE*)malloc(ImgSize);
+	fread(Image, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+
+	int Histo[256] = { 0 };
+	int AHisto[256] = { 0 };
+	int H = hInfo.biHeight, W = hInfo.biWidth;
+
+	Binarization(Image, Temp, W, H, 50);
+	InverseImage(Temp, Temp, W, H);
+	m_BlobColoring(Temp, H, W);
+
+	int x1, x2, y1, y2;
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			if (Temp[i * W + j] == 0) {
+				y1 = i;
+				break;
+			}
+		}
+	}
+	for (int i = H-1; i >= 0; i--) {
+		for (int j = W-1; j >= 0; j--) {
+			if (Temp[i * W + j] == 0) {
+				y2 = i;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < W; i++) {
+		for (int j = 0; j < H; j++) {
+			if (Temp[j * W + i] == 0) {
+				x2 = i;
+				break;
+			}
+		}
+	}
+	for (int i = W - 1; i >= 0; i--) {
+		for (int j = H - 1; j >= 0; j--) {
+			if (Temp[j * W + i] == 0) {
+				x1 = i;
+				break;
+			}
+		}
+	}
+
+	int Cx=0, Cy=0;
+	for (int i = x1; i <= x2; i++) Cx += i;
+	Cx /= (x2 - x1 + 1);
+	for (int i = y2; i <= y1; i++) Cy += i;
+	Cy /= (y1 - y2 + 1);
+
+
+	DrawRectOutline(Image, W, H, x1, x2, y1, y2);
+	DrawCrossLine(Image, W, H, Cx, Cy);
+
+
+	SaveBMPFile(hf, hInfo, hRGB, Image, hInfo.biWidth, hInfo.biHeight, "pupil2_result.bmp");
+
+	free(Image);
+	free(Output);
+	free(Temp);
+	return 0;
+}
